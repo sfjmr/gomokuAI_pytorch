0db556dd6591a7ca094c2788adefb10f93c14c78
kawasemiwho <fujimori.shu.sf@gmail.com>
2019-02-03 21:45:00 +0900

    memory lengthを増やした

--- init.py
+++ init.py
@@ -11,9 +11,9 @@ device = ("cuda" if torch.cuda.is_available() else "cpu")
 
 GAMMA = 0.999  # 時間割引率
 NUM_EPISODES = 50  # 最大試行回数 これを行うごとにネットワークを比較する
-BATCH_SIZE = 100
-epoch_num = 1  # 学習する回数
-CAPACITY = 10000
+BATCH_SIZE = 10000
+epoch_num = 10  # 学習する回数
+CAPACITY = 400000
 lr = 0.01  # 学習係数 初期値
 T = 0.1  # 逆温度　初期値
 pool_num = 1  # 並列処理
