693dca2e63d2890f0462228001aef026c846b78c
kawasemiwho <fujimori.shu.sf@gmail.com>
2019-02-05 12:51:53 +0900

    fix

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ if __name__ == '__main__':
 
                     
                     #print("-1*reward", -1*reward)
-                    memory.push(tmp_data[-2][0], tmp_data[-2][1], None, reward)
+                    memory.push(tmp_data[-2][0], tmp_data[-2][1], None, -1*reward)
                     memory.push(tmp_data[-4][0], tmp_data[-4][1], None, -1*GAMMA*reward)
                     
                     break #whileを抜ける
