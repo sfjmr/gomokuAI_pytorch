c4b3463547b49f074e025db5fd2c8c30c47ffc32
kawasemiwho <fujimori.shu.sf@gmail.com>
2019-02-08 11:57:40 +0900

    test

--- brain.py
+++ brain.py
@@ -273,8 +273,8 @@ class Brain_dqn:
             state_batch = torch.cat(batch.state)
             action_batch = torch.cat(batch.action)
             reward_batch = torch.cat(batch.reward)
-            #print(batch.put_available_position)
-            print(batch.reward.size())
+            print(batch.put_available_position[0])
+            print(batch.reward[0])
             put_available_position_batch = torch.cat(batch.put_available_position)
             
             
