0f97975e362c071b5d6c65ae6c684828de1464c2
kawasemiwho <fujimori.shu.sf@gmail.com>
2019-03-24 22:26:40 +0900

    fix

--- brain.py
+++ brain.py
@@ -220,7 +220,7 @@ class Brain_dqn:
             q = self.rtn_q(ban, model, player_side, action)
             index = rc2index(action[0], action[1])
             #print(i, action, q)
-            p_ary[index] = q*1 + p_ary[index]*0
+            p_ary[index] = q*0.99 + p_ary[index]*(1-0.99)
         #print(p_ary)
         
         
